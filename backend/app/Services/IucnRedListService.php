559c8c7fc5ee2296855d5e30deb770c532fb2d4f
Fortunato De Angelis <deangelis.fortunato@gmail.com>
2023-07-07 21:00:53 +0200

    feat: 🎉adds getNumPageForRegionalAssessments method in IucnRedList services

--- backend/app/Services/IucnRedListService.php
+++ backend/app/Services/IucnRedListService.php
@@ -10,6 +10,7 @@ class IucnRedListService
     CONST URL = 'https://apiv3.iucnredlist.org/api/';
     // Token sample for test
     CONST TOKEN = '9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee';
+    CONST MAX_RESULTS_PER_PAGE = 10000; // Max results per page as per API documentation
 
     protected $client;
 
@@ -34,5 +35,21 @@ class IucnRedListService
         ]);
 
         return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * Get number of pages for a specific region
+     * @param string $region
+     * @return int
+     */
+    public function getNumPageForRegionalAssessments(string $region): int
+    {
+        $response = $this->client->request('GET', 'speciescount/region/' . $region, [
+            'query' => [
+                'token' => self::TOKEN,
+            ],
+        ]);
+        $result =  json_decode($response->getBody()->getContents(), true);
+        return (int) ceil($result['count'] / self::MAX_RESULTS_PER_PAGE);
     }
 }
