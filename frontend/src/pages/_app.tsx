fc01ff6e2d63f34ce705a69723544b0d2be9b9c9
Fortunato De Angelis <deangelis.fortunato@gmail.com>
2023-07-08 01:11:26 +0200

    chore: 🔧 removes global css and introduces matine provider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { MantineProvider } from '@mantine/core';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      theme={{
+        colorScheme: 'light',
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
