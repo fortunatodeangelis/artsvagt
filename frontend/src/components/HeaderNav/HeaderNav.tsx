92ba6ac4f3c7ee163bf4e92670cb2aa1fa6d22e3
Fortunato De Angelis <deangelis.fortunato@gmail.com>
2023-07-09 21:02:44 +0200

    feat: 🎉 adds link on logo components

--- frontend/src/components/HeaderNav/HeaderNav.tsx
+++ frontend/src/components/HeaderNav/HeaderNav.tsx
@@ -11,17 +11,19 @@ export function HeaderNav() {
   return (
     <Header height={56} mb={120}>
       <Container className={classes.inner}>
-        <Logo />
+        <Link href="/">
+          <Logo />
+        </Link>
 
         <Group spacing={0} className={classes.social} position="right" noWrap>
           <ActionIcon size="lg">
-            <Link 
+            <Link
               className={classes.link}
               href="https://github.com/fortunatodeangelis/artsvagt"
               target="_blank"
               rel="noopener noreferrer"
             >
-            <BrandGithub size="1.1rem" />
+              <BrandGithub size="1.1rem" />
             </Link>
           </ActionIcon>
         </Group>
