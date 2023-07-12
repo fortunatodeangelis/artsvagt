57992ecf5bd7853ded2276671a5ae74d1b147b8e
Fortunato De Angelis <deangelis.fortunato@gmail.com>
2023-07-12 20:21:44 +0200

    chore: ✨refactors GetSpeciesByRegion controller. Retrives data from store

--- backend/app/Http/Controllers/GetSpeciesByRegion.php
+++ backend/app/Http/Controllers/GetSpeciesByRegion.php
@@ -4,48 +4,68 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use App\Services\IucnRedListService;
+use App\Models\Species;
 
 class GetSpeciesByRegion extends Controller
 {
-    public function __construct(IucnRedListService $iucnService)
-    {
-        $this->iucnService = $iucnService;
-    }
 
+    const PER_PAGE = 15;
+
+    /**
+     * Get species by region.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index(Request $request)
     {
-        // Validate the request
         $validator = Validator::make($request->all(), [
             'region' => 'required|string',
             'page' => 'integer',
-            'category' => 'string|nullable',
-            'class' => 'string|nullable',
+            'category' => 'string',
+            'class' => 'string',
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
 
-        // Check if the region is valid
-        $regions = $this->iucnService->getRegions();
-        $region = $request->region;
+        $region = $request->input('region');
 
-        if (!in_array($region, array_column($regions['results'], 'identifier'))) {
-            return response()->json(['error' => 'Invalid region'], 400);
+        // Get the page number.
+        $page = $request->input('page') ?? 1;
+
+        $species = Species::where('regions_identifier', $region);
+
+        // If category is set, filter by category.
+        if ($request->has('category') && $request->input('category') !== 'all') {
+            $category = $request->input('category');
+            $species = $species->where('category', $category);
         }
 
-        $response = $this->iucnService->getSpeciesByRegion(
-            $request->region,
-            $request->page ?? 0,
-            $request->category ?? 'all',
-            $request->class ?? 'all'
-        );
-
-        if (empty($response)) {
-            return response()->json(['error' => 'No species found'], 404);
+        // If class is set, filter by class.
+        if ($request->has('class') && $request->input('class') !== 'all') {
+            $class = $request->input('class');
+            $species = $species->where('class_name', $class);
         }
 
-        return response()->json($response);
+        // Get the number of pages.
+        $count = $species->count();
+        $pages = ceil($count / self::PER_PAGE);
+
+        $species = $species->skip(($page - 1) * self::PER_PAGE)->take(self::PER_PAGE)->get();
+
+        // If no species found, return 404.
+        if ($species->isEmpty()) {
+            return response()->json(['message' => 'No species found for this region.'], 404);
+        }
+
+        // Return the species and the number of pages.
+        return response()->json([
+            'species' => $species,
+            'pages' => $pages,
+            'count' => $count,
+            'currentPage' => $page
+        ]);
     }
 }
